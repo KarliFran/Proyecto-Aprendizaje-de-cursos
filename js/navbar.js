f8ebba87c9d6fcd743f8de42a15fcc14b76948ca
ksilveira <silveirak908@gmail.com>
2021-08-16 10:35:58 -0400

    first commit

---
+++ js/navbar.js
@@ -0,0 +1,7 @@
+function accion(){
+    console.log('funciona');
+    let ancla = document.getElementsByClassName('nav-item');
+    for (var i = 0; i < ancla.length; i++ ){
+        ancla[i].classList.toggle('desaparece');
+    }
+}
